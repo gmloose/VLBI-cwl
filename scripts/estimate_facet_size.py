0341e469f211b0c3b029b8133a75644c71a34ead
Frits Sweijen <tikk3r@users.noreply.github.com>
2025-02-10 15:15:39 +0000

    Write output to JSON for easier parsing

--- scripts/estimate_facet_size.py
+++ scripts/estimate_facet_size.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+import json
 from argparse import ArgumentParser
 
-from regions import Regions
 import numpy as np
+from regions import Regions
+
 
 def calculate_image_size(ras, decs, pixel_size, padding: float = 1.0):
     width_ra = abs(max(ras) - min(ras))
@@ -10,8 +12,15 @@ def calculate_image_size(ras, decs, pixel_size, padding: float = 1.0):
     dec_centre = (min(decs) + max(decs)) / 2
 
     pix_size_deg = pixel_size / 3600
-    imwidth, imheight = width_ra * np.cos(np.deg2rad(dec_centre)) / pix_size_deg, width_dec / pix_size_deg
-    return padding * imwidth, padding * imheight
+    imwidth, imheight = (
+        width_ra * np.cos(np.deg2rad(dec_centre)) / pix_size_deg,
+        width_dec / pix_size_deg,
+    )
+
+    imsize = max(imwidth, imheight)
+    blavg = 1.5e3 * 60000.0 * 2.0 * 3.1415 * 1.5 / (24.0 * 60.0 * 60 * imsize)
+    return padding * imwidth, padding * imheight, blavg
+
 
 def main():
     parser = ArgumentParser(
@@ -24,16 +33,19 @@ def main():
         "--pixel_size", type=float, help="Pixel size of the image to be made."
     )
     parser.add_argument(
-        "--padding", type=float, help="Padding factor to pad the calculated image size with. This allows some extra freedom in tweaking the final image size."
+        "--padding",
+        type=float,
+        help="Padding factor to pad the calculated image size with. This allows some extra freedom in tweaking the final image size.",
     )
     args = parser.parse_args()
 
     reg = Regions.read(args.region, format="ds9")
     ra = reg[0].vertices.ra.value
     dec = reg[0].vertices.dec.value
-    width, height = calculate_image_size(ra, dec, args.pixel_size, args.padding)
-    with open("bouding_box.txt", "w") as f:
-        f.write(f"{width} {height}")
+    width, height, blavg = calculate_image_size(ra, dec, args.pixel_size, args.padding)
+    with open("bouding_box.json", "w") as f:
+        json.dump({"width": width, "height": height, "blavg": blavg}, f)
+
 
 if __name__ == "__main__":
     main()
